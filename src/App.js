5cd0ef2aca575c41ddccd4d387624cb30d8ba11f
Siyeon <gkatldus1@gmail.com>
2021-08-21 23:13:08 +0900

    add remove, select function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.child = React.createRef();
+  }
+
   state = {
     maxNo: 3,
     boards: [
@@ -19,38 +24,62 @@ class App extends Component {
     ]
   }
 
+  // handleSaveData = (data) => {
+  //   this.setState({
+  //     maxNo: this.state.maxNo+1,
+  //     boards: this.state.boards.concat({ brdno: this.state.maxNo, brddate: new Date(), ...data })
+  //   });
+  // }
   handleSaveData = (data) => {
-    this.setState({
-      maxNo: this.state.maxNo+1,
-      boards: this.state.boards.concat({ brdno: this.state.maxNo, brddate: new Date(), ...data })
-    });
+    let boards = this.state.boards;
+    if (data.brdnno === null || data.brdno === '' || data.brdno === undefined) {
+      this.setState({
+        maxNo: this.state.maxNo+1,
+          boards: boards.concat({brdno: this.state.maxNo, brddate: new Date(), brdwriter: data.brdwriter, brdtitle: data.brdtitle })
+      });
+    } 
+    else {
+      this.setState({
+        boards: boards.map(row => data.brdno === row.brdno ? { ...data }: row)
+      })
+    }
   }
 
-render() {
-  const { boards } = this.state;
-  
-  return (
-    <div>
-      <BoardForm onSaveData={this.handleSaveData}/>
-      <table border="1">
-        <tbody>
-          <tr align="center">
-            <td width="50">No.</td>
-            <td width="300">Title</td>
-            <td width="100">Name</td>
-            <td width="100">Date</td>
-          </tr>
-            { 
-              boards.map(row => (
-                <BoardItem key={row.brdno} row={row}/>
-              ))
-            }
-        </tbody>
-      </table>
-    </div>
-    );
+  handleRemove = (brdno) => {
+    this.setState({
+      boards: this.state.boards.filter(row => row.brdno !== brdno)
+    })
   }
-}
+
+  handleSelectRow = (row) => {
+    this.child.current.handleSelectRow(row);
+  }
+
+  render() {
+    const { boards } = this.state;
+    
+    return (
+      <div>
+        <BoardForm onSaveData={this.handleSaveData} ref={this.child}/>
+        <table border="1">
+          <tbody>
+            <tr align="center">
+              <td width="50">No.</td>
+              <td width="300">Title</td>
+              <td width="100">Name</td>
+              <td width="100">Date</td>
+            </tr>
+              { 
+                boards.map(row => (
+                  <BoardItem key={row.brdno} row={row} onRemove={this.handleRemove} onSelectRow={this.handleSelectRow}/>
+                ))
+              }
+          </tbody>
+        </table>
+      </div>
+      );
+    }
+  }
 
 class BoardItem extends React.Component {
   render() {
@@ -89,8 +118,6 @@ class BoardForm extends Component {
       </form>
     )
   }
-
-
 }
 
 
